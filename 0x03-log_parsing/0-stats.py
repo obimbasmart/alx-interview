d2ede3928001d67ba17363bbef0ff139156c0587
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-01 05:59:49 +0100

    initial commit

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import re
+import sys
+from collections import defaultdict
+import signal
+
+log_format_regex = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' \
+    r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" ' \
+    r'(200|301|400|401|403|404|405|500) (\d+)$'
+
+
+# register signal handler for keyboard interrupt
+def signal_handler(sig, frame):
+    pass
+
+
+def get_log_info(line: str) -> dict:
+    __match = re.match(log_format_regex, line)
+    return {
+        "is_valid_server_log": bool(bool(__match)),
+        "status_code": __match.group(3),
+        "file_size": int(__match.group(4))
+    }
+
+
+def print_stats(stat: dict):
+    """print statistics"""
+    print(f'File size: {stat["file_size"]}')
+
+    [
+        print(f'{status_code}: {count}')
+        for status_code, count in stat.items()
+        if status_code != "file_size"
+    ]
+
+
+def log_parse():
+    """readline and computes metrics"""
+    metrics = defaultdict(int)
+
+    for idx, line in enumerate(sys.stdin):
+
+        if (idx + 1) % 10 == 0:
+            print_stats(metrics)
+
+        log = get_log_info(line)
+        if (log["is_valid_server_log"]):
+            metrics[log["status_code"]] += 1
+            metrics["file_size"] += log["file_size"]
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    log_parse()
